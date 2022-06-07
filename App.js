58e312a37f327cc8bddd34482ff5df06c496bc7c
Hudson <hudsonsretonde@gmail.com>
2022-06-07 10:29:55 -0300

    Projeto final.

--- App.js
+++ App.js
@@ -1,62 +1,86 @@
-import React from 'react'
-import {View, StyleSheet, TouchableOpacity} from 'react-native'
-
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
 
 const App = () => {
-  const toggle = false;
-  return <View style={toggle ? style.containerLight : style.container}>
-    <TouchableOpacity onPress={() => ()}>
+  const [toggle, setToggle] = useState(false);
 
-    <Image style={ toggle ? style.lightingOn : style.lightingOff}
-    source={
-      toggle
-      ? require('./assets/icons/eco-ligth.png')
-      : require('./assets/icons/eco-ligth-off.png')
-    }/>
-    <Image style={style.dioLogo}
-    source={
-      toggle
-      ? require('./assets/icons/logo-dio.png')
-      : require('./assets/icons/logo-dio-white.png')
-    }/>
-    </TouchableOpacity>
-  </View>
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(() => {
+    // Liga flash do dispositivo
+    Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect(() => {
+    /**
+     * Quando o celular for chacoalhado, muda-se o toggle
+     */
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+
+    // Essa func vai ser chamada quando o componets for desmontado
+    return () => subscription.remove();
+  }, []);
+
+  return (
+    <View style={toggle ? style.containerLight : style.container}>
+      <TouchableOpacity onPress={handleChangeToggle}>
+        <Image
+          style={toggle ? style.lightingOn : style.lightingOff}
+          source={
+            toggle
+              ? require('./assets/icons/eco-light.png')
+              : require('./assets/icons/eco-light-off.png')
+          }
+        />
+        <Image
+          style={style.dioLogo}
+          source={
+            toggle
+              ? require('./assets/icons/logo-dio.png')
+              : require('./assets/icons/logo-dio-white.png')
+          }
+        />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
-export default App
+export default App;
 
 const style = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  containerLight:{
-    flex:1,
-    backgroundColor: 'black',
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  lightingOn:{
+  lightingOn: {
     resizeMode: 'contain',
     alignSelf: 'center',
     width: 150,
     height: 150,
   },
-  lightingOff:{
+  lightingOff: {
     resizeMode: 'contain',
     alignSelf: 'center',
     tintColor: 'white',
     width: 150,
     height: 150,
   },
-  dioLogo:{
+  dioLogo: {
     resizeMode: 'contain',
     alignSelf: 'center',
-    tintColor: 'white',
     width: 250,
     height: 250,
   },
-    
 });
